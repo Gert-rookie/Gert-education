6e9e6f720204cf641fa51a7a867c6440be0ca1f1
Gert <winone@gmail.com>
2020-04-06 20:15:45 +0800

    将cms管理添加至注册中心

--- gert-service/gert-service-cms/src/main/java/top/fotg/educms/CmsApplication.java
+++ gert-service/gert-service-cms/src/main/java/top/fotg/educms/CmsApplication.java
@@ -3,10 +3,12 @@ package top.fotg.educms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"top.fotg"}) //指定扫描位置
+@EnableDiscoveryClient
 @MapperScan("top.fotg.educms.mapper")
 public class CmsApplication {
     public static void main(String[] args) {
