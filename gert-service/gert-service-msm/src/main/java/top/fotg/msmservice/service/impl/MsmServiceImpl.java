0edb6984da761e1cfdc70407b4170f1ac31f1c3f
Gert <winone@gmail.com>
2020-05-10 13:53:41 +0800

    数据库改成本地的库

--- gert-service/gert-service-msm/src/main/java/top/fotg/msmservice/service/impl/MsmServiceImpl.java
+++ gert-service/gert-service-msm/src/main/java/top/fotg/msmservice/service/impl/MsmServiceImpl.java
@@ -22,7 +22,7 @@ public class MsmServiceImpl implements MsmService {
         if(StringUtils.isEmpty(phone)) return false;
 
         DefaultProfile profile =
-                DefaultProfile.getProfile("default", "LTAI0QLzHyqmUaDV", "ovnXfw7JrLiTBlVAKZghzMWrYoiVaO");
+                DefaultProfile.getProfile("default", "", "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         //设置相关固定的参数
